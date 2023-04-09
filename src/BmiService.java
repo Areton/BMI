2da6c06574efe533cfd5bb5698835b76d2aa969a
Никитосик <nik-ar98@mail.ru>
2023-04-09 11:34:09 +0500

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+
+    public int calculate(int weight, double height) {
+        int bmi;
+        bmi = (int) (weight / (height * height));
+        return bmi;
+
+    }
+}
